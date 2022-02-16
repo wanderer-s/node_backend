bed19df1fde771bb4400e08ab58c31eb4b068ee5
whatseop <ows0871@gmail.com>
2022-02-17 00:08:48 +0900

    base entity에 ApiProperty 추가

--- src/common/entities/base.entity.ts
+++ src/common/entities/base.entity.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class Base {
+  @ApiProperty({ description: 'id' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ description: 'data 생성 일시' })
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
+  @ApiProperty({ description: 'data 수정 일시' })
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
 }
