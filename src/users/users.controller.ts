98f05b6826c04a0cdc12b479400768a36d46bb38
whatseop <ows0871@gmail.com>
2022-02-17 17:25:33 +0900

    Check nickname or email dulicate

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiInternalServerErrorResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Query, Post } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiInternalServerErrorResponse, ApiOkResponse, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { UserSignUpDto } from './dto/userSignUp.dto';
 
@@ -21,5 +21,36 @@ export class UsersController {
   @Post('signup')
   async userSignUp(@Body() data: UserSignUpDto) {
     await this.usersService.userSignUp(data);
+  }
+
+  @ApiOperation({
+    summary: 'email 및 nickname 중복확인',
+    description: 'email 또는 nickname 둘중 하나의 값만 입력 (동시에 입력 X)',
+  })
+  @ApiQuery({
+    description: 'email 중복여부',
+    name: 'email',
+    required: false,
+    example: 'test@test.com',
+  })
+  @ApiQuery({
+    description: 'nickname 중복여부',
+    name: 'nickname',
+    required: false,
+    example: 'nickname',
+  })
+  @ApiOkResponse({
+    description: 'OK',
+    schema: { type: 'object', properties: { data: { type: 'string', enum: ['exists', 'none'] } } },
+  })
+  @Get('info')
+  async checkInfo(@Query('email') email: string, @Query('nickname') nickname: string) {
+    if (email) {
+      const result = await this.usersService.getUser({ email });
+      return result ? { data: 'exists' } : { data: 'none' };
+    } else if (nickname) {
+      const result = await this.usersService.getUser({ nickname });
+      return result ? { data: 'exists' } : { data: 'none' };
+    }
   }
 }
