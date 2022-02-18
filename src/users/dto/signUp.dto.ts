e171d494835544883be5cdb05d827a3e485223c6
whatseop <ows0871@gmail.com>
2022-02-19 01:07:13 +0900

    Rename Dtos

--- src/users/dto/signUp.dto.ts
+++ src/users/dto/signUp.dto.ts
@@ -1,8 +1,8 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
-import { Users } from '../entities/users.entity';
+import { Users } from 'src/users/entities/users.entity';
 
-export class UserSignUpDto extends PickType(Users, ['email', 'nickname', 'password'] as const) {
+export class SignUpDto extends PickType(Users, ['email', 'nickname', 'password'] as const) {
   @IsNotEmpty()
   @IsString()
   @ApiProperty({ description: '비밀번호 확인' })
